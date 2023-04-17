d037bcf0e24ea76b7bf1739480fc144c44ff4e20
Youssef Zidan <youssefzidan.yz@gmail.com>
2023-04-17 07:26:47 +0200

    align data stuff

--- prepare_data.py
+++ prepare_data.py
@@ -4,4 +4,12 @@ import os
 
 data_root = "/home/elab/Documents/data_Youssef/10x_ds_scans_orig_AIMOS/"
 
-print(os.listdir(data_root))
+mice = sorted(os.listdir(data_root))
+
+train_mice = mice[:12]
+test_mice = mice[12:]
+
+val_mice = train_mice[11:]
+train_mice = train_mice[:11]
+
+pics = sorted(glob.glob(os.path.join(data_root, train_mice[0], "C00", "*"))) # C00 C01 label
