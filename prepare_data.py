18753d7dd6c6bbc9824f694770664dd2d25b1f62
Youssef Zidan <youssefzidan.yz@gmail.com>
2023-06-07 15:35:35 +0200

    generate new file names for paired data

--- prepare_data.py
+++ prepare_data.py
@@ -15,25 +15,30 @@ test_mice = mice[12:]
 val_mice = train_mice[11:]
 train_mice = train_mice[:11]
 
+index = 0
+
 
 def copy_set(folder_names, set_label):
-
+    global index
+    print("Copying " + set_label)
     for folder in tqdm(folder_names):
         pics_A = sorted(glob.glob(os.path.join(data_root, folder, "C00", "*")),
                         key=lambda x: x.split("/")[-1].split(".")[0])  # C00 C01 label
         pics_B = sorted(glob.glob(os.path.join(data_root, folder, "C01", "*")),
                         key=lambda x: x.split("/")[-1].split(".")[0])
 
-        print("Copying " + set_label)
-        for pic_A, pic_B in zip(pics_A, pics_B):
-            filename = pic_A.split("/")[-1]
-            if "-" in filename:
-                dst_filename = filename.split("-")[-1]
-            else:
-                dst_filename = filename.split("_")[-1]
+        pics_C = sorted(pics_B = sorted(glob.glob(os.path.join(data_root, folder, "label", "*")),
+                                        key=lambda x: x.split("/")[-1].split(".")[0]))
+
+        for pic_A, pic_B, pic_C in zip(pics_A, pics_B, pics_C):
+            dst_filename = f"{index}.tif"
+            dst_filename_label = f"{index}.tiff"
 
             shutil.copy(pic_A, os.path.join(destination, "A", set_label, dst_filename))
             shutil.copy(pic_B, os.path.join(destination, "B", set_label, dst_filename))
+            shutil.copy(pic_C, os.path.join(destination, "C", set_label, dst_filename_label))
+            
+            index += 1
 
 
 copy_set(train_mice, "train")
