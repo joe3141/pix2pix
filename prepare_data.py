91769d916e1a868040bc42fcd38d66b98b9ff370
Youssef Zidan <youssefzidan.yz@gmail.com>
2023-06-07 13:46:09 +0200

    fixed param in prepare data

--- prepare_data.py
+++ prepare_data.py
@@ -17,9 +17,9 @@ train_mice = train_mice[:11]
 
 
 def copy_set(folder_names, set_label):
-    pics_A = sorted(glob.glob(os.path.join(data_root, train_mice[0], "C00", "*")),
+    pics_A = sorted(glob.glob(os.path.join(data_root, folder_names[0], "C00", "*")),
                     key=lambda x: x.split("/")[-1].split(".")[0])  # C00 C01 label
-    pics_B = sorted(glob.glob(os.path.join(data_root, train_mice[0], "C01", "*")),
+    pics_B = sorted(glob.glob(os.path.join(data_root, folder_names[0], "C01", "*")),
                     key=lambda x: x.split("/")[-1].split(".")[0])
 
     print("Copying " + set_label)
