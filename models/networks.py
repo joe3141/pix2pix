4786d558fad0da34b7f719bc35b41064344a4f7b
Youssef Zidan <youssefzidan.yz@gmail.com>
2023-08-15 03:26:11 +0200

    atrous conv disc

--- models/networks.py
+++ models/networks.py
@@ -584,6 +584,38 @@ class NLayerDiscriminator(nn.Module):
         return self.model(input)
 
 
+# class PixelDiscriminator(nn.Module):
+#     """Defines a 1x1 PatchGAN discriminator (pixelGAN)"""
+#
+#     def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d):
+#         """Construct a 1x1 PatchGAN discriminator
+#
+#         Parameters:
+#             input_nc (int)  -- the number of channels in input images
+#             ndf (int)       -- the number of filters in the last conv layer
+#             norm_layer      -- normalization layer
+#         """
+#         super(PixelDiscriminator, self).__init__()
+#         if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+#             use_bias = norm_layer.func == nn.InstanceNorm2d
+#         else:
+#             use_bias = norm_layer == nn.InstanceNorm2d
+#
+#         self.net = [
+#             nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
+#             norm_layer(ndf * 2),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)]
+#
+#         self.net = nn.Sequential(*self.net)
+#
+#     def forward(self, input):
+#         """Standard forward."""
+#         return self.net(input)
+
+
 class PixelDiscriminator(nn.Module):
     """Defines a 1x1 PatchGAN discriminator (pixelGAN)"""
 
@@ -601,16 +633,47 @@ class PixelDiscriminator(nn.Module):
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
 
-        self.net = [
-            nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
+        self.net1 = nn.Sequential(
+            nn.Conv2d(input_nc, ndf, kernel_size=4, stride=2, bias=use_bias),
+            # norm_layer(ndf),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
-            norm_layer(ndf * 2),
+            nn.Conv2d(ndf, ndf * 2, kernel_size=4, stride=2, bias=use_bias),
+#             norm_layer(ndf * 2),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)]
+            nn.Conv2d(ndf * 2, ndf * 4, kernel_size=4, stride=2, bias=use_bias),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)
+            )
 
-        self.net = nn.Sequential(*self.net)
+        self.net2 = nn.Sequential(
+            nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=2),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=4),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True),
+            # nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=8),
+#             # norm_layer(ndf * 4),
+            # nn.LeakyReLU(0.2, True),
+        )
+
+        self.merge = nn.Sequential(
+            nn.Conv2d(ndf * 8, ndf * 4, kernel_size=1, stride=1, padding="same", bias=use_bias),
+            nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True),
+        )
+
+        self.lsgan = nn.Conv2d(ndf * 4, 1, kernel_size=1, stride=1, bias=use_bias)
+
 
     def forward(self, input):
         """Standard forward."""
-        return self.net(input)
+        x = self.net1(input)
+        x2 = self.net2(x)
+        merge = torch.cat([x, x2], dim=1)
+        clean = self.merge(merge)
+        return self.lsgan(clean)
