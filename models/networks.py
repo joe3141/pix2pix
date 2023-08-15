eeb75ba28c1b209d52abf27f424a22c21ccad5ec
Youssef Zidan <youssefzidan.yz@gmail.com>
2023-08-16 01:52:15 +0200

    added atrous disc

--- models/networks.py
+++ models/networks.py
@@ -566,10 +566,41 @@ class NLayerDiscriminator(nn.Module):
             return self.model(input)
 
 
+# class PixelDiscriminator(nn.Module):
+#     """Defines a 1x1 PatchGAN discriminator (pixelGAN)"""
+#
+#     def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d):
+#         """Construct a 1x1 PatchGAN discriminator
+#
+#         Parameters:
+#             input_nc (int)  -- the number of channels in input images
+#             ndf (int)       -- the number of filters in the last conv layer
+#             norm_layer      -- normalization layer
+#         """
+#         super(PixelDiscriminator, self).__init__()
+#         if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+#             use_bias = norm_layer.func == nn.InstanceNorm2d
+#         else:
+#             use_bias = norm_layer == nn.InstanceNorm2d
+#
+#         self.net = [
+#             nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
+#             norm_layer(ndf * 2),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)]
+#
+#         self.net = nn.Sequential(*self.net)
+#
+#     def forward(self, input):
+#         """Standard forward."""
+#         return self.net(input)
+
 class PixelDiscriminator(nn.Module):
     """Defines a 1x1 PatchGAN discriminator (pixelGAN)"""
 
-    def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d):
+    def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d, use_sigmoid=False, getIntermFeat=True):
         """Construct a 1x1 PatchGAN discriminator
 
         Parameters:
@@ -578,21 +609,138 @@ class PixelDiscriminator(nn.Module):
             norm_layer      -- normalization layer
         """
         super(PixelDiscriminator, self).__init__()
+        self.getIntermFeat = getIntermFeat
         if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
             use_bias = norm_layer.func == nn.InstanceNorm2d
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
 
-        self.net = [
-            nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
-            nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
-            norm_layer(ndf * 2),
-            nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)]
+        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=4, stride=2, bias=use_bias),
+            nn.LeakyReLU(0.2, True)]]
 
-        self.net = nn.Sequential(*self.net)
+
+        sequence += [[nn.Conv2d(ndf, ndf * 2, kernel_size=4, stride=2, bias=use_bias),
+#             norm_layer(ndf * 2),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence += [[nn.Conv2d(ndf * 2, ndf * 4, kernel_size=4, stride=2, bias=use_bias),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)]]
+
+
+        sequence += [[nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence2 = [[nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=2),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence2 += [[nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=4),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence2 += [[nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=8),
+#             # norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence3 = [[nn.Conv2d(ndf * 8, ndf * 4, kernel_size=1, stride=1, padding="same", bias=use_bias),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence3 += [[nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias),
+#             norm_layer(ndf * 4),
+            nn.LeakyReLU(0.2, True)]]
+
+        sequence3 += [[nn.Conv2d(ndf * 4, 1, kernel_size=1, stride=1, bias=use_bias)]]
+
+        if getIntermFeat:
+            i = 0
+            for n in range(len(sequence)):
+                setattr(self, 'model'+str(i), nn.Sequential(*sequence[n]))
+                i += 1
+
+            for n in range(len(sequence2)):
+                setattr(self, 'model'+str(i), nn.Sequential(*sequence2[n]))
+                i += 1
+
+            for n in range(len(sequence3)):
+                setattr(self, 'model'+str(i), nn.Sequential(*sequence3[n]))
+                i += 1
+
+        else:
+            sequence_stream, sequence_stream2, sequence_stream3 = [], [], []
+            for n in range(len(sequence)):
+                sequence_stream += sequence[n]
+            self.net1 = nn.Sequential(*sequence_stream)
+
+            for n in range(len(sequence2)):
+                sequence_stream += sequence2[n]
+            self.net2 = nn.Sequential(*sequence_stream2)
+
+            for n in range(len(sequence3)):
+                sequence_stream += sequence3[n]
+            self.net3 = nn.Sequential(*sequence_stream3)
+
+#         self.net1 = nn.Sequential(
+#             nn.Conv2d(input_nc, ndf, kernel_size=4, stride=2, bias=use_bias),
+#             # norm_layer(ndf),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf, ndf * 2, kernel_size=4, stride=2, bias=use_bias),
+# #             norm_layer(ndf * 2),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 2, ndf * 4, kernel_size=4, stride=2, bias=use_bias),
+# #             norm_layer(ndf * 4),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias),
+# #             norm_layer(ndf * 4),
+#             nn.LeakyReLU(0.2, True)
+#             )
+#
+#         self.net2 = nn.Sequential(
+#             nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=2),
+# #             norm_layer(ndf * 4),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=4),
+# #             norm_layer(ndf * 4),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias, dilation=8),
+# #             # norm_layer(ndf * 4),
+#             nn.LeakyReLU(0.2, True),
+#         )
+#
+#         self.merge = nn.Sequential(
+#             nn.Conv2d(ndf * 8, ndf * 4, kernel_size=1, stride=1, padding="same", bias=use_bias),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(ndf * 4, ndf * 4, kernel_size=3, stride=1, padding="same", bias=use_bias),
+# #             norm_layer(ndf * 4),
+#             nn.LeakyReLU(0.2, True),
+#         )
+#
+#         self.lsgan = nn.Conv2d(ndf * 4, 1, kernel_size=1, stride=1, bias=use_bias)
+
 
     def forward(self, input):
         """Standard forward."""
-        return self.net(input)
+
+        if self.getIntermFeat:
+            first = None
+            res = [input]
+            for n in range(10):
+                model = getattr(self, "model" + str(n))
+                x = res[-1]
+                if n == 7:
+                    x = torch.cat([first, x], dim=1)
+                y = model(x)
+                res.append(y)
+                if n == 3:
+                    first = y
+            return res[1:]
+
+        else:
+            x = self.net1(input)
+            x2 = self.net2(x)
+            merge = torch.cat([x, x2], dim=1)
+            clean = self.net3(merge)
+            return clean
+
+
